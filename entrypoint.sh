46e92596d0cc36e2ad8cb38467c4267ff39bff05
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-02-19 07:37:19 +0200

    Force password update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 if ! id "$FTP_USER" &>/dev/null; then
     echo "Creating FTP user: $FTP_USER"
     useradd -m -d /var/ftp -s /usr/sbin/nologin $FTP_USER
-    echo "$FTP_USER:$FTP_PASS" | chpasswd
 fi
+echo "$FTP_USER:$FTP_PASS" | chpasswd
 
 # Set up FTP directories
 mkdir -p /var/ftp/unprocessed /var/ftp/booklets
